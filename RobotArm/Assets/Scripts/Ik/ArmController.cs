646f4dce868771485f70f16f1184cc2476cdbab6
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-05 10:07:35 -0500

    Wrist offset and inverse applied

--- RobotArm/Assets/Scripts/Ik/ArmController.cs
+++ RobotArm/Assets/Scripts/Ik/ArmController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6065b45b5452b29cd166cedc2e2d6d8c69d6c00edafa7d7b50b03f42f9d0d551
-size 2778
+oid sha256:269e47ef58910b1a37d48ac92bac6a4e3ca21789ac80f2f801ba171d310d3b5f
+size 3002
