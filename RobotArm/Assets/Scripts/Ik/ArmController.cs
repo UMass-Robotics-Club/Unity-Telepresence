931ba4a213b0e3c54cae50f6441f086b504d4c0b
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-05 18:15:36 -0500

    New colors and Ik experiment

--- RobotArm/Assets/Scripts/Ik/ArmController.cs
+++ RobotArm/Assets/Scripts/Ik/ArmController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:269e47ef58910b1a37d48ac92bac6a4e3ca21789ac80f2f801ba171d310d3b5f
-size 3002
+oid sha256:6554ac7fdcc26ba4064515ab36a01aa6ed3fa55822860d5774e54e4d80a4afc6
+size 2995
