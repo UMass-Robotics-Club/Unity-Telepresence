cd33e5a3e1abe1a5ef4e1a45b3649ef710a9257a
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-16 17:40:27 -0500

    Import Models and reorganized them

--- RobotArm/Assets/Scripts/Ik/ArmController.cs
+++ RobotArm/Assets/Scripts/Ik/ArmController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6554ac7fdcc26ba4064515ab36a01aa6ed3fa55822860d5774e54e4d80a4afc6
-size 2995
+oid sha256:3ad0a4a132a54807896926e3de33593ef2cc42299383816ebc4850d8b553e1c8
+size 4570
