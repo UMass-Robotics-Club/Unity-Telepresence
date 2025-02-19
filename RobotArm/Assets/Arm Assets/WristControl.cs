bbe1ad7fcc2d73fc2e9fc51989ea0cd51ffa3a8c
idealmonke <ty913913@yahoo.com>
2025-02-19 10:28:55 -0500

    Removed extra parts to increase performance

--- RobotArm/Assets/Arm Assets/WristControl.cs
+++ RobotArm/Assets/Arm Assets/WristControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cb4a43c1cdd1a58a6cad409ac9198262abbc9f3613441106606a93d6d4d550d
-size 3221
+oid sha256:914148c3721db2c11b0c535fb4b117170bcf746129c90d9b661ff079e8ba2af8
+size 3580
