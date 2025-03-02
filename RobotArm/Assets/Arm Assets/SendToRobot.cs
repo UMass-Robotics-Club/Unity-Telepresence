5bd8f992fffa004fef13d6bb9ee8fee638d3ee36
idealmonke <ty913913@yahoo.com>
2025-03-02 14:06:16 -0500

    Added script to store arm values

---
+++ RobotArm/Assets/Arm Assets/SendToRobot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29eb1cad68ba9172ab9b9e48e7af3be64fbfce405df9bedcbc1e4eea6fad740e
+size 593
--- RobotArm/Assets/Arm Assets/WristControl.cs
+++ RobotArm/Assets/Arm Assets/WristControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:914148c3721db2c11b0c535fb4b117170bcf746129c90d9b661ff079e8ba2af8
-size 3580
+oid sha256:a77cd17b981f29d75492d3f0c6e8061813bfc8b165a940e2ad432f53bc061781
+size 5289
