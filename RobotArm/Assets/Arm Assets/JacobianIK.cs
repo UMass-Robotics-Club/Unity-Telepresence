16a7a00353273933f480307d95c63825464d2414
idealmonke <ty913913@yahoo.com>
2025-04-13 13:58:33 -0400

    RobotController Controls all IK

--- RobotArm/Assets/Arm Assets/JacobianIK.cs
+++ RobotArm/Assets/Arm Assets/JacobianIK.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e103df1cc3d324d90644220e9f108d7c19501d8e6a796831b419a1495bc75cd2
-size 6315
+oid sha256:14c83c0e812fbfa515f671ef2d6e1cba5c521ad65ff0b7bcf29d92446afb9604
+size 6214
--- RobotArm/Assets/Arm Assets/RobotController.cs
+++ RobotArm/Assets/Arm Assets/RobotController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d237c4b67fcc020800bc06568ac94f21b94a40d7d7777be133628d0b84b20d1
-size 597
+oid sha256:c8a9b6e14980d5ac3af5823e784e97a719ed4138ed7129d0e549c1591fa261d2
+size 756
